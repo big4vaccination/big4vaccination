cc863701d56c06e8c0e414b6f6f966c01a009a00
keepdreamlive <635538043@qq.com>
2019-08-23 19:38:43 +1000

    Add admin already
    Setting up db, and add me as superuser.

--- big4Vaccination/settings.py
+++ big4Vaccination/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'cool_vaccine', # here is the app's name
+    'cool_vaccine.apps.CoolVaccineConfig', # here is the app's name
 ]
 
 MIDDLEWARE = [
--- cool_vaccine/models.py
+++ cool_vaccine/models.py
@@ -5,22 +5,32 @@ from django.contrib.auth.models import User
 from django.utils import timezone
 
 # Create your models here.
-class vaccine_compare(models.Model):
+class Vaccine_Compare(models.Model):
     # vaccine_name
     vaccine_name = models.CharField(max_length=100)
     # the time of taking vaccination
-    vaccine_time = models.IntegerField(50)
+    vaccine_time = models.IntegerField(3)
     # short_name of vaccine
-    short_name_vaccine = models.CharField(max_length = 20)
+    short_name_vaccine = models.CharField(max_length=20)
     # description
     description = models.TextField(max_length=200)
     # country
-    country = models.CharField(max_length = 100)
+    country = models.CharField(max_length=100)
     # is a child
     is_child = models.BooleanField(default=False)
 
+    # This is a default to show the vaccine_name
+    def __str__(self):
+        return self.vaccine_name
+    # Default to show the month
+    def __int__(self):
+        return self.vaccine_time
+    # Default to show the flag
+    def __bool__(self):
+        return self.is_child
+
 # Create GP entity
-class GP(models.Model):
+class GP_Info(models.Model):
     # address
     address = models.TextField(max_length=200)
     # phone_number
