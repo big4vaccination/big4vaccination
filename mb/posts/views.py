615645be0ca180354f55c8a6a8a0d77ef79bbfcf
KEEPDREAMALIVE <47722428+KEEPDREAMALIVE@users.noreply.github.com>
2019-09-17 15:38:40 +0800

    Dont show the 0

--- mb/posts/views.py
+++ mb/posts/views.py
@@ -146,19 +146,14 @@ def advanced_searched(request):
     if country_name:
         ## Generating push content for first table.
         for i in range(len(data_disease)):
-            push_disease[i]["Disease name "] = data_disease[i][1]
-            if data_disease[i][2] is None:
-                push_disease[i]["Number of reported cases in " + str(country_name) + ""] = 0
-            else:
+            if data_disease[i][2] and data_disease[i][5]:
+                push_disease[i]["Disease name "] = data_disease[i][1]
                 push_disease[i]["Number of reported cases in " + str(country_name) + ""] = data_disease[i][2]
             # push_disease[i]["Average immunisation coverage"]= data_disease[i][3]
-            push_disease[i]["Percentage of reported cases in " + str(country_name) + ""] = data_disease[i][4]
+                push_disease[i]["Percentage of reported cases in " + str(country_name) + ""] = data_disease[i][4]
             # push_disease[i]["Average immunisation coverage in AU"]= data_disease[i][3]
-            if data_disease[i][5] is None:
-                push_disease[i]["Number of reported cases in Australia"] = 0
-            else:
                 push_disease[i]["Number of reported cases in Australia"] = data_disease[i][5]
-            push_disease[i]["Percentage of reported cases in Australia"] = data_disease[i][7]
+                push_disease[i]["Percentage of reported cases in Australia"] = data_disease[i][7]
 
             push_disease.append({})
 
